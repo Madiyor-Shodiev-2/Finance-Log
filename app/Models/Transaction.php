162fb1f20bb76a83b3d5bdf2f12afa07c53f3847
Rayid32 <madiyorshodiev333@gmail.com>
2025-05-20 20:45:09 +0500

    MINOR: Added migrations and seeders

---
+++ app/Models/Transaction.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    /** @use HasFactory<\Database\Factories\TransactionFactory> */
+    use HasFactory;
+
+    protected $table = "transactions";
+
+    protected $fillable =
+        [
+            "amount",
+            "date",
+            "category_id",
+        ];
+
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
