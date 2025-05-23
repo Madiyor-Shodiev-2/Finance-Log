87f53cb9a15910a0d5d2eae0694f309749300f52
Rayid32 <madiyorshodiev333@gmail.com>
2025-05-23 14:23:18 +0500

    UPDATE: Request and Controller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -18,14 +18,16 @@ class TransactionController extends Controller
      */
     public function index()
     {
-
         $id = Auth::user()->id;
+
         $transactions = TransactionSummaryService::getAll($id);
 
-        $categories = Category::all();
+        $descriptions = Transaction::select('description')
+            ->where('user_id', '=', $id)
+            ->get();
 
         return view('main.transactions.index', [
-            'categories' => $categories,
+            'descriptions' => $descriptions,
             'daily' => $transactions['daily'],
             'monthly' => $transactions['monthly'],
             'weekly' => $transactions['weekly']
@@ -37,23 +39,13 @@ class TransactionController extends Controller
     public function store(StoreTransactionRequest $request)
     {
         if(!Auth::check()){
-            return redirect()->back()->withErrors(['auth' => 'Пожалуйста, зарегистрируйтесь или войдите в систему, чтобы добавить транзакцию.']);
-        }
-
-        //Shu yerdan balans bilan ishlash kerak
-
-        /* Agarda Expense bo'lsa,  unda balansdan olib tashlaydi va real blansga qo'shib qo'yadi
-         * Agar balans yetarli bo'lmasa, tranzaksiyani balansiga haqiqiy summa qoshib qoyadi (20-30=0)
-         * balance hech qachon minus son olmaydi
-         * va qolgan summani real balancga qo'shib qo'yadi (20-30=-10) -10 ni real balansga qo'shib qo'yadi
-         * Agar balance 0 dan kichik bo'lsa, unda real balansga qo'shib qo'yadi
-         * 
-         * Agar balans yetarli bo'lsa, unda balansdan olib tashlaydi (10-10 = 0)
-         *  Agar qarz bo'lsa, va qarz kategoriyaga to'lov ketmagan bo'lsa, unda balansdan ayrib tashaydi (10-10=0) 
-         *  real balansdan esa (0-10=-10) (-10-10=-20)
-         * */
-
-        
+            return redirect()
+            ->back()
+            ->withErrors([
+                'auth' => 'Пожалуйста, зарегистрируйтесь или войдите в систему, чтобы 
+                добавить транзакцию.'
+            ]);
+        } // TODO: Возможно это не пригодится  
 
         $id = Auth::user()->id;
 
@@ -63,18 +55,19 @@ class TransactionController extends Controller
             'user_id'     => $id,
             'amount'      => $data['amount'],
             'description' => $data['description'],
-            'date'        => $data['date'] ?? now(),
+            'date'        => now(),
             'type'        => $data['type'],
         ]);
         
         UserBalanseAction::execute(Auth::user(), $transaction);
 
-        return redirect()->back();
+        return redirect()->route('home');
     }
 
     public function daily()
     {
-        $id = Auth::user()->id; //fix it
+        $id = Auth::user()->id;
+        
         $daily = TransactionSummaryAll::getDaily($id);
         $dailyBalance = TransactionSummaryService::getDaily($id);
 
@@ -85,7 +78,7 @@ class TransactionController extends Controller
     }
     public function weekly()
     {
-        $id = Auth::user()->id; //fix me   
+        $id = Auth::user()->id;   
         
         $weekly = TransactionSummaryAll::getWeekly($id);
         $week = TransactionSummaryService::getWeekly($id);
@@ -97,7 +90,7 @@ class TransactionController extends Controller
     }
     public function monthly()
     {   
-        $id = Auth::user()->id; //fix it
+        $id = Auth::user()->id;
 
         $monthly = TransactionSummaryAll::getMonthly($id);
         $month = TransactionSummaryService::getMonthly($id);
